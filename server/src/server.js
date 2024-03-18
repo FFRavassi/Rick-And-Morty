5e4ffa58474f952685ec8bad1d83ee2d82fec935
FFRavassi <francescoravassi@gmail.com>
2024-03-18 02:28:50 -0300

    main

---
+++ server/src/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+
+const server = express();
+
+server.use(morgan("dev"));
+
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+  });
+  
+server.use(express.json());
+
+module.exports = server;
